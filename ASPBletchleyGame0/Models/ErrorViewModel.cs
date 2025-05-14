78018fa315edf58c6282f374d977a9b8b4013a33
tkrsvtdrv <tkrsvtdrv>
2025-05-14 15:01:53 +0300

    Add project files.

---
+++ ASPBletchleyGame0/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPBletchleyGame0.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
